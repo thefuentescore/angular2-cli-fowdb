2e74fc3281aa00a48232da6a131379c3a4557dcf
sfuentes <sfuentes@net4things.com>
2017-04-20 18:00:37 +0200

    Starting to route the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CardModule } from "./card/card.module";
 import { NavigationModule } from "./navigation/navigation.module";
+
+
 
 @NgModule({
   declarations: [
--- src/app/card/card.module.ts
+++ src/app/card/card.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from '@angular/router';
 
 import { CardListComponent } from './card-browser/card-list/card-list.component';
 import { CardBrowserComponent } from './card-browser/card-browser.component';
@@ -10,8 +11,16 @@ import { RaceSortPipe } from './card-browser/card-list/race-sort.pipe';
 import { CardService } from "./card.service";
 import { SidenavComponent } from './card-browser/sidenav/sidenav.component';
 
+const appRoutes: Routes = [
+  { path: 'card-browser', component: CardBrowserComponent }
+];
+
 @NgModule({
-  imports: [ CommonModule, FormsModule ],
+  imports: [ 
+    CommonModule, 
+    FormsModule,
+    RouterModule.forRoot(appRoutes) 
+  ],
   declarations: [ CardListComponent, CardBrowserComponent, NameSortPipe, RaceSortPipe, SidenavComponent ],
   providers: [ CardService ],
   exports: [CardListComponent, CardBrowserComponent]
